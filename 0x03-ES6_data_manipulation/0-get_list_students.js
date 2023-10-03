c6850b3068935b9eb359c6f123faf2f8d40e86f9
Reen25 <mumodoreen25@gmail.com>
2023-10-03 08:36:56 +0000

    list students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Returns a list of students
+ * @returns {{id: Number, firstName: string, location: string}[]}
+ */
+export default function getListStudents(){
+	return [
+		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+		{ id: 2, firstName: 'James', location: 'Columbia' },
+		{ id: 5, firstName: 'Serena', location: 'San Francisco' },
+	];
+}
+	
