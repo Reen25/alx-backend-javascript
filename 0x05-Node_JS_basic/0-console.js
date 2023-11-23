3b2e9eb468d3cd41dae8635898e8c64fc7545d94
Reen25 <mumodoreen25@gmail.com>
2023-11-23 08:31:43 +0000

    console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Doreen <https://github.com/Reen25>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
